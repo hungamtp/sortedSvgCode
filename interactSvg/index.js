64b588356f9d879fa0a0a90db9c5ad622909b088
Hung.NguyenBa <hung.nguyenba@sts.saigontechnology>
2022-09-29 11:31:28 +0700

    one more text

--- interactSvg/index.js
+++ interactSvg/index.js
@@ -1,6 +1,10 @@
 const textLove = document.getElementById("text");
+const textLove1 = document.getElementById("text1");
+const textLove2 = document.getElementById("text2");
 
 textLove.innerHTML =  String.fromCharCode(9617)
+textLove1.innerHTML =  String.fromCharCode(9617)
+textLove2.innerHTML =  String.fromCharCode(9617)
 function moveRandomNess ( ){
   var a =Math.random() < 0.5;
   var b =Math.random() > 0.5;
@@ -30,48 +34,65 @@ function moveRandomNess ( ){
     }
   }
 }
-setInterval(moveRandomNess , 100)
+function moveRandomNess1 ( ){
+  var a =Math.random() < 0.5;
+  var b =Math.random() > 0.5;
+  if(a){
+    if(b){
+      let valueXIncrease = textlove1.x["animVal"][0].value +5
+      textlove1.style.x = valueXIncrease;
+      textlove1.setAttribute("x" , valueXIncrease);
+      textlove1.innerHTML = String.fromCharCode(9617)
+    }else{
+      let valueXIncrease = textlove1.x["animVal"][0].value -5;
+      textlove1.style.x = valueXIncrease;
+      textlove1.setAttribute("x" , valueXIncrease);
+      textlove1.innerHTML = String.fromCharCode(9618)
+    }
+  }else{
+    if(b){
+      let valueXIncrease = textlove1.y["animVal"][0].value +5
+      textlove1.style.y = valueXIncrease;
+      textlove1.setAttribute("y" , valueXIncrease);
+      textlove1.innerHTML = String.fromCharCode(9619)
+    }else{
+      let valueXIncrease = textlove1.y["animVal"][0].value -5
+      textlove1.style.y = valueXIncrease;
+      textlove1.setAttribute("y" , valueXIncrease);
+      textlove1.innerHTML = String.fromCharCode(9620)
+    }
+  }
+}
+function moveRandomNess2 ( ){
+  var a =Math.random() < 0.5;
+  var b =Math.random() > 0.5;
+  if(a){
+    if(b){
+      let valueXIncrease = textLove2.x["animVal"][0].value +5
+      textlove2.style.x = valueXIncrease;
+      textlove2.setAttribute("x" , valueXIncrease);
+      textlove2.innerHTML = String.fromCharCode(9617)
+    }else{
+      let valueXIncrease = textlove2.x["animVal"][0].value -5;
+      textlove2.style.x = valueXIncrease;
+      textlove2.setAttribute("x" , valueXIncrease);
+      textlove2.innerHTML = String.fromCharCode(9618)
+    }
+  }else{
+    if(b){
+      let valueXIncrease = textlove2.y["animVal"][0].value +5
+      textlove2.style.y = valueXIncrease;
+      textlove2.setAttribute("y" , valueXIncrease);
+      textlove2.innerHTML = String.fromCharCode(9619)
+    }else{
+      let valueXIncrease = textlove2.y["animVal"][0].value -5
+      textlove2.style.y = valueXIncrease;
+      textlove2.setAttribute("y" , valueXIncrease);
+      textlove2.innerHTML = String.fromCharCode(9620)
+    }
+  }
+}
+setInterval(moveRandomNess , 10)
+setInterval(moveRandomNess1 , 10)
+setInterval(moveRandomNess2 , 10)
 
-// const textLove = document.getElementById("text");
-// let increase = true;
-// 
-
-// function increaseX ( ){
-//    ;
-//   
-//  ;
-//   if(valueXIncrease == 100){
-//     clearInterval(intervalIncreaseX);
-//     intervalY = setInterval(increaseY , 10);
-//     increase = false;
-//   }
-// }
-
-// function decreaseX ( ){
-//   let valueXIncrease = Number( textLove.style.x) -1 ;
-//   textLove.style.x = valueXIncrease;
-//   textLove.setAttribute("x" , valueXIncrease);
-//   if(valueXIncrease == 2){
-//     clearInterval(intervalDecreaseX)
-//     intervalY = setInterval(increaseY , 10);
-//     increase = true;
-//   }
-// }
-// function increaseY ( ){
-//   let valueXIncrease = Number( textLove.style.y) +1 ;
-//   textLove.style.y = valueXIncrease;
-//   textLove.setAttribute("y" , valueXIncrease);
-//   if(valueXIncrease == currentY +35){
-//     currentY = valueXIncrease;
-//     clearInterval(intervalY);
-//     if(increase){
-//       intervalIncreaseX = setInterval(increaseX , 10);
-//     }else{
-//       intervalDecreaseX = setInterval(decreaseX , 10);
-//     }
-    
-//   }
-// }
-// var intervalIncreaseX = setInterval(increaseX , 10);
-// var intervalY ;
-// var intervalDecreaseX ;
